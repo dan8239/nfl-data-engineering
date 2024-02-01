a3f125652157b50b5c832e70d45694143b8bbdba
dan8239 <daniel.joseph.pfeiffer@gmail.com>
2024-01-31 23:02:23 -0600

    TR scraper working for first 4 URLS. Keep going!

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import asyncio
 
-from web_scrapers.team_rankings import predictive_rankings_scraper
+from web_scrapers.team_rankings import team_rankings_scraper
 
 
 async def run():
     # df = await vsin_scraper.get_vsin_game_lines()
     date = "10-15-2023"
-    scr = predictive_rankings_scraper.PredictiveRankingsScraper()
-    df = scr.get_table(date=date)
+    scr = team_rankings_scraper.TeamRankingsScraper()
+    df = scr.get_all_tables_for_date(date=date)
     print(df)
 
 
--- src/web_scrapers/team_rankings/team_rankings_scraper.py
+++ src/web_scrapers/team_rankings/team_rankings_scraper.py
@@ -1,3 +1,4 @@
+import os
 import ssl
 
 import pandas as pd
@@ -5,10 +6,12 @@ import pandas as pd
 
 class TeamRankingsScraper:
     def __init__(self):
+        print()
+        print(os.getcwd())
         ssl._create_default_https_context = ssl._create_unverified_context
-        self.base_url = None
-        self.cols_to_keep = None
-        self.naming_prefix = None
+        self.url_df = pd.read_excel(
+            "web_scrapers/team_rankings/urls_team_rankings.xlsx"
+        )
 
     def _strip_team_names(self, df):
         """
@@ -28,33 +31,97 @@ class TeamRankingsScraper:
         df["date"] = date
         return df
 
+    def _split_win_loss(self, df, columns):
+        for col in columns:
+            df[[f"{col}_wins", f"{col}_losses"]] = df[col].str.split("-", expand=True)
+            df[[f"{col}_wins", f"{col}_losses"]] = df[
+                [f"{col}_wins", f"{col}_losses"]
+            ].astype(int)
+            df[f"{col}_games_played"] = df[f"{col}_wins"] + df[f"{col}_losses"]
+            df.drop(columns=col, inplace=True)
+        return df
+
     def _col_names_to_lower_case(self, df):
         df.columns = map(str.lower, df.columns)
         return df
 
-    def _add_prefixes_to_col_names(self, df, prefix, skip_cols):
+    def _add_prefixes_to_col_names(self, df, prefix, cols_to_process):
         df = df.rename(
             columns={
-                col: prefix + col if col not in skip_cols else col for col in df.columns
+                col: prefix + col if col in cols_to_process else col
+                for col in df.columns
             }
         )
         return df
 
-    def get_table(self, date):
+    def _drop_spaces_in_col_names(self, df):
+        df.columns = df.columns.str.replace(" ", "")
+        return df
+
+    def __get_table(
+        self, category, table_name, base_url, cols_to_keep, record_cols, date
+    ):
         """
-        get a table from a teamrankings site and format it
+        Get a single table from a team rankings site and process it into a dataframe
+
+        Args:
+            category (str): category of table (off, def, st, etc.)
+            table_name (str): table name (predictive, yards, tds, etc.)
+            base_url (str): url of the table
+            cols_to_keep (list(str)): which columns to keep in the table
+            date (str): date value to get table data from (yyyy-mm-dd)
+            record_cols (list(str)): cols w/ records (3-1) that need s  ome processing
 
         Returns:
-            pd.DataFrame: cleansed dataframe
+            pd.DataFrame: formatted table w/ date and colnames adjusted
         """
-        url = self.base_url.format(date)
+        url = f"{base_url}?date={date}"
+        print(f"getting {url}")
         tables = pd.read_html(url)
         df = tables[0]
-        df = self._add_date_to_df(df, date)
         df = self._strip_team_names(df)
+        df = df[["Team"] + cols_to_keep]
+        df = self._split_win_loss(df, columns=record_cols)
         df = self._col_names_to_lower_case(df)
-        df = df[self.cols_to_keep]
+        df = self._drop_spaces_in_col_names(df)
         df = self._add_prefixes_to_col_names(
-            df=df, prefix=self.naming_prefix, skip_cols=["date", "team"]
+            df=df,
+            prefix=f"{category}_{table_name}_",
+            cols_to_process=df.drop(columns="team").columns,
         )
         return df
+
+    def get_all_tables_for_date(self, date):
+        """_summary_
+
+        Args:
+            date (_type_): _description_
+        """
+        all_stats_df = pd.DataFrame()
+        for i, row in self.url_df.iterrows():
+            keep_cols = [
+                element.strip()
+                for element in row.cols_to_keep.split(",")
+                if element.strip()
+            ]
+            record_cols = [
+                element.strip()
+                for element in row.record_cols.split(",")
+                if element.strip()
+            ]
+            df = self.__get_table(
+                category=row.category,
+                table_name=row.table_name,
+                base_url=row.base_url,
+                cols_to_keep=keep_cols,
+                record_cols=record_cols,
+                date=date,
+            )
+            if all_stats_df.empty:
+                all_stats_df = df
+            else:
+                all_stats_df = pd.merge(
+                    left=all_stats_df, right=df, how="left", on="team"
+                )
+        all_stats_df = self._add_date_to_df(df, date)
+        return all_stats_df
