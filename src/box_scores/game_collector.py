2bdf6c6db5ee4c222963078c4f0864bd240bbf3b
dan8239 <daniel.joseph.pfeiffer@gmail.com>
2024-05-07 23:59:01 -0500

    Box score collector done

--- src/box_scores/game_collector.py
+++ src/box_scores/game_collector.py
@@ -1,16 +1,28 @@
 import dotenv
-import sportsdataverse
+import pandas as pd
+import sportsdataverse as sdv
 
 dotenv.load_dotenv()
 
 
 class GameCollector:
     def __init__(self):
-        print("test some shit jackass")
-        test = sportsdataverse.nfl.espn_nfl_schedule(
-            dates=2023, week=1, season_type=2, limit=20, return_as_pandas=True
-        )
-        print(test.head())
+        self.df = None
 
-    def collect():
-        print("test some shit jackass")
+    def collect(self):
+        all_seasons_list = []
+        for year in range(2005, 2024):
+            print(f"collecting {year}")
+            season = sdv.nfl.espn_nfl_schedule(dates=year, return_as_pandas=True)
+            print(season.head())
+            all_seasons_list.append(season)
+            df = pd.DataFrame(season)
+            df.to_csv(f"../output/box_scores/{year}_box_scores.csv", index=False)
+        self.df = pd.concat(all_seasons_list)
+        print("saving full df")
+        self.df.to_csv("../output/box_scores/all_box_scores.csv", index=False)
+
+
+if __name__ == "__main__":
+    gc = GameCollector()
+    gc.collect()
